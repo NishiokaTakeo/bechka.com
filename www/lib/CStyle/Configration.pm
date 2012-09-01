a26c69b5c326f8b8b88c25b96abe882b77c0269b
takeo_nishioka <takeo.1983@hotmail.com>
2012-09-01 23:48:23 +0900

    ドキュメントルート、ログ、テンプレート等のパスが環境によっても影響を受けないように変更。 ※Apacheドキュメントルートから取得するように変更
    不要なコメントや空白・インデントを削除

--- www/lib/CStyle/Configration.pm
+++ www/lib/CStyle/Configration.pm
@@ -1,51 +1,31 @@
-﻿package CStyle::Configration ;
+package CStyle::Configration ;
 
 BEGIN{
 eval("use lib qw($ENV{DOCUMENT_ROOT}lib);");
 };
 use strict;
-#$BEGIN{
-#eval("use lib qw($ENV{DOCUMENT_ROOT}lib);");
-#};use lib qw(/home/bechka/www/lib) ;
 
 ##########################################################
 # データ宣言
 ##########################################################
 
 # DB関係
-our $gv_dbname				= 'bechka' ;
-our $gv_dbhost 				= 'localhost' ;
-our $gv_dbuser				= 'bechka' ;
-our $gv_password			= 'bechpass' ;
-our $template			= '/home/bechka/www/html' ;	
-our $gv_errorLogPath 		= '/home/bechka/logs/trace.log' ;
+our $USER_PATH      = &get_user_path($ENV{DOCUMENT_ROOT});
+our $DOCUMENT_ROOT  = $USER_PATH. '/www/' ;
+our $LOGS_ROOT      = $USER_PATH. '/logs/' ;
+our $gv_dbname      = 'bechka' ;
+our $gv_dbhost      = 'localhost' ;
+our $gv_dbuser      = 'bechka' ;
+our $gv_password    = 'bechpass' ;
+our $template       = $DOCUMENT_ROOT . 'html' ;	
+our $gv_errorLogPath	= $LOGS_ROOT . '/trace.log' ;
 #our $gv_libPath 		= '/public_html/lib' ;
 our $is_debug 		= '0' ;
-our $DBAccess;
-my $Company_Address		= 'info@cluster-style.com' ;
 
 sub get_temp_path{
 	return $CStyle::Configration::template ;
 }
 
-=pod
-sub New{
-
-  my $class = shift(@_);
-  
-  my $self = {
-		SmtpServer				=> 'localhost' ,
-		CompanyAddress			=> 'info@cluster-style.com' ,
-		Domain					=> 'cluster-style.com'  ,
-		smtp_auth_id			=> 'nishioka@cluster-style.com'  ,
-		smtp_auth_pass			=> '#Gamk3b.' ,
-		_template_dir			=> '/home/cs/www/cluster-style.com/template/'
-		
-  };
-  
-  return bless $self,$class;
-}
-=cut
 sub get_negative_symbol{
 	CStyle::Common::print_log('I',"START") ;
 	return 'N' ;
@@ -54,6 +34,15 @@ sub get_negative_symbol{
 sub get_positive_symbol{
 	CStyle::Common::print_log('I',"START") ;
 	return 'P' ;
+}
+
+sub get_user_path {   
+    my $doc_root = shift ;
+
+    my @dirs = split('/',$doc_root) ;
+    pop(@dirs);
+
+    return join('/',@dirs) ;
 }
 
 1;
